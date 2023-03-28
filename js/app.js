51a6e7ab6c9f33322cebeb78e40d94c70d17957a
cameron <hammerbro_97@hotmail.com>
2023-03-28 17:03:32 -0600

    added random extras

--- js/app.js
+++ js/app.js
@@ -29,7 +29,9 @@ function mouseOver(background){
 
 
     background[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
+    background[`target`][`style`][`background-position`] = `50%`, `50%`;
 
+    background[`target`][`style`][`object-fit`] = `cover`;
 };
 
 function mouseOff(none){
@@ -55,7 +57,7 @@ for(let counter = 0; counter < bidooftime.length; counter = counter +1){
 
 function addBidoof(bidoof){
   
-
+    bidoof[`target`][`style`][`background-position`] = `50%`, `50%`;
     bidoof[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
 
 
@@ -74,7 +76,8 @@ function addBidoof2(bidoof2){
   
 
     bidoof2[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
-
+    bidoof2[`target`][`style`][`background-position`] = `50%`, `50%`;
+    bidoof2[`target`][`style`][`object-fit`] = `cover`;
 
 };
 
@@ -115,3 +118,58 @@ function offSpace(bigdoofus){
 let turn_off = document.querySelector(`body`);
 
 body.addEventListener(`keydown`, offSpace);
+
+
+
+
+
+
+//this is just extra stuff I added afterwards for fun don't worry about it//
+
+
+
+
+
+function change_the_image(image){
+
+    if(image[`target`].getAttribute(`src`) === `/images/399.png`){
+    
+        image[`target`].setAttribute(`src`, `https://www.serebii.net/swordshield/pokemon/399.png`);
+    
+    }else{
+    
+        image[`target`].setAttribute(`src`, `/images/399.png`);
+    }
+    
+    };
+    
+    let change_image = document.querySelectorAll(`.page_span`);
+    
+    for(counter = 0; counter < change_image.length; counter = counter +1){
+    
+    change_image[counter].addEventListener(`click`, change_the_image)
+    
+    
+    };
+
+
+    function image_BG(biggest){
+
+
+        biggest[`target`][`style`][`background-image`] = `url(https://lh3.googleusercontent.com/Gtx0rIY2cjEjh2gf3uN1U9QfUjVuJO7I7VtenD_9poaMo9V6Z-YQw5d2pwqBJovuR52tPaApwUjYFINWetjjrrYOJIhJXGaSm0z-1h1xJsHXAOw=s2400-rj)`;
+        bidoof2[`target`][`style`][`background-position`] = `50%`, `50%`;
+        bidoof2[`target`][`style`][`object-fit`] = `cover`;
+    
+
+    };
+
+
+    let image_background = document.querySelectorAll(`.images`);
+
+  
+    for(counter = 0; counter < image_background.length; counter = counter +1){
+
+        image_background[counter].addEventListener(`click`, image_BG)
+
+
+    };
