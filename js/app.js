7b0a7f68e191c5ed590a24e401b0b0ac98896438
cameron <hammerbro_97@hotmail.com>
2023-03-27 20:24:02 -0600

    added additional functions and events

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function makeNew(detail_1){
     detail_1[`target`][`style`][`color`] = `#7798AB`;
 
 
-}
+};
 
 
 //this function just changes the color//
@@ -30,13 +30,13 @@ function mouseOver(background){
 
     background[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
 
-}
+};
 
 function mouseOff(none){
 
 
     none[`target`][`style`][`background-image`] = `none`;
-}
+};
 
 let bidooftime = document.querySelectorAll(`.page_span`);
 
@@ -49,31 +49,6 @@ for(let counter = 0; counter < bidooftime.length; counter = counter +1){
 
 };
 
-
-//i dont know how to make this work at all//
-
-function onSpace(main){
-
-    if(main[`code`] === `Space`){
-        main[`target`][`style`][`background-color`] = `black`;
-        }
-return false;
-
-
-}
-
-let change_body = document.querySelectorAll(`.page_span`);
-
-for(let counter = 0; counter < change_body.length; counter = counter +1){
-
-
-    change_body[counter].addEventListener(`keydown`, onSpace);
-
-
-
-};
-
-
 //more background images that appear on dblclicks//
 
 
@@ -84,7 +59,7 @@ function addBidoof(bidoof){
     bidoof[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
 
 
-}
+};
 
 let add_bidoof = document.querySelector(`#page_header`);
 
@@ -101,8 +76,40 @@ function addBidoof2(bidoof2){
     bidoof2[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
 
 
-}
+};
 
 let add_bidoof2 = document.querySelector(`#page_footer`);
 
 add_bidoof2.addEventListener(`dblclick`, addBidoof2);
+
+
+
+
+
+function onSpace(bigdoof){
+
+if(bigdoof[`code`] === `Space` ) {
+
+    bigdoof[`target`][`style`][`background-image`] = `url(https://assets.pokemon.com/assets/cms2/img/pokedex/full/399.png)`;
+}
+
+};
+
+let body = document.querySelector(`body`);
+
+body.addEventListener(`keydown`, onSpace);
+
+
+
+function offSpace(bigdoofus){
+
+    if(bigdoofus[`code`] === `Enter` ) {
+
+        bigdoofus[`target`][`style`][`background-image`] = `none`;
+    }
+
+}
+
+let turn_off = document.querySelector(`body`);
+
+body.addEventListener(`keydown`, offSpace);
